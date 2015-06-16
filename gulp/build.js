f4cd69980a8265034340e5e2a0358b0001d6fb44
cameron boyer <cameron.boyer@gmail.com>
2015-06-16 14:58:18 -0400

    uglify
    the uglifyer was breaking the dist build

--- gulp/build.js
+++ gulp/build.js
@@ -43,7 +43,7 @@ module.exports = function(options) {
       .pipe($.rev())
       .pipe(jsFilter)
       .pipe($.ngAnnotate())
-      .pipe($.uglify({ preserveComments: $.uglifySaveLicense })).on('error', options.errorHandler('Uglify'))
+      // .pipe($.uglify())
       .pipe(jsFilter.restore())
       .pipe(cssFilter)
       .pipe($.csso())
