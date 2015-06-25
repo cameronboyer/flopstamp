17f417c3e91c72340d68e437746d43791ef835cf
cameron boyer <cameron.boyer@gmail.com>
2015-06-25 10:30:43 -0400

    login attempts

--- src/app/index.js
+++ src/app/index.js
@@ -3,13 +3,24 @@
 var flop;
 (function (flop) {
     'use strict';
-    angular.module('flop', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ngMaterial'])
+    angular.module('flop', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize',
+        'ngResource', 'ngRoute', 'ngMaterial', 'oauth'])
         .controller('FlopCtrl', flop.FlopCtrl)
-        .config(function ($routeProvider) {
+        .config(function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
             templateUrl: 'app/flop/flop.html',
             controller: 'FlopCtrl'
+        })
+            .when('/access_token=:accessToken', {
+            templateUrl: 'app/flop/welcome.html',
+            controller: function ($location, AccessToken) {
+                var hash = $location.path().substr(1);
+                AccessToken.setTokenFromString(hash);
+                $location.path('/');
+                $location.replace();
+                console.log("whats location?" + $location);
+            }
         })
             .otherwise({
             redirectTo: '/'
